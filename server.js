14061aaa137226700f42145c1efb5c767c88efd9
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-09 14:35:46 -0400

    test push

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 //requiring the data (route is going to require data from this file) 
 const { animals } = require('./data/animals.json');
